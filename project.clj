dc9eec604cd84fee11a2764c0adc1270dd44f280
David Altenburg <daltenburg@lmd-daltenbur01.local>
2010-08-07 17:27:19 -0500

    Upgraded Clojure to 1.2 RC Added location data parsing for bus info

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject gotbs "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]
+  :dependencies [[org.clojure/clojure "1.2.0-RC2"]
+                 [org.clojure/clojure-contrib "1.2.0-RC2"]
 		 [ring "0.1.1-SNAPSHOT"]
 		 [enlive "1.0.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.2.1"]])
--- src/gotbs/bustracker.clj
+++ src/gotbs/bustracker.clj
@@ -1,7 +1,10 @@
 (ns gotbs.bustracker
   (:import
    (java.net URL)
-   (java.io BufferedReader InputStreamReader)))
+   (java.io BufferedReader InputStreamReader StringBufferInputStream))
+  (:use
+   clojure.xml (parse)
+   clojure.java.io (input-stream)))
 
 (defn fetch-url
   "Returns the contents at a URL as a string"
@@ -11,7 +14,29 @@
       (let [buf (BufferedReader. (InputStreamReader. stream))]
 	(apply str (line-seq buf))))))
 
-(defn fetch-location-data []
+(defn fetch-location-data-xml []
   (fetch-url 
    "http://www.ctabustracker.com/bustime/api/v1/getvehicles?key=kv6yHNkUrkZJkjA8u7V5sxNTq&rt=72,50"))
 
+(defn vehicle-content-xml-to-map
+	[xml]
+	(let [
+		xml-to-map
+		(fn [coll tag-attrs]
+			(assoc 
+				coll 
+				(:tag tag-attrs) 
+				(let [content (:content tag-attrs)]
+					(if (> (count content) 1)
+						content
+						(first content)))))]
+		(reduce xml-to-map {} xml)))
+		
+
+
+(defn make-vehicles
+	[xml]
+	(map vehicle-content-xml-to-map (map :content (filter #(= (:tag %) :vehicle) (xml-seq xml)))))
+	
+(defn fetch-location-data []
+  (make-vehicles (parse (StringBufferInputStream. (fetch-location-data-xml)))))
