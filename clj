894a0a033e9be0eb5c572464d281a93f1feeed1c
David Altenburg <daltenburg@lmd-daltenbur01.local>
2010-08-18 23:44:33 -0500

    Using enlive to process templates - step one. Nothing dynamic yet

--- clj
+++ clj
@@ -2,7 +2,7 @@
 
 LIBS=`printf "%s:" lib/*.jar | sed s/:$//`
 if [ $# -eq 0 ]; then
-   java -cp $LIBS:src clojure.main
+   java -cp $LIBS:src:resources clojure.main
 else
-  java -cp $LIBS:src clojure.main $1 - $@
+    java -cp $LIBS:src:resources clojure.main $1 - $@
 fi	
--- src/gotbs/core.clj
+++ src/gotbs/core.clj
@@ -1,7 +1,11 @@
 (ns gotbs.core
   (:use gotbs.web.locations)
   (:use ring.middleware.file)
-  (:use ring.middleware.file-info))
+  (:use ring.middleware.file-info)
+  (:use net.cgrand.enlive-html))
+
+(deftemplate locations "web/index.html" [a]
+  [:div#main] (content a))
 
 (defn router [req]
   (condp = (:uri req)
@@ -13,13 +17,25 @@
 		   :body (str "This is Ring on " (:server-name req))}
       "/locations" {:status 200
 		    :headers {"Content-Type" "text/html"}
+		    :body (apply str (locations "SUBSTITUTED!"))}
+      "/locations2" {:status 200
+		    :headers {"Content-Type" "text/html"}
 		    :body (do-get req)}
       {:status 200
        :headers {"Content-Type" "text/html"}
        :body (str "Welcome")}))
 
+(def *dummy-context*
+     [{
+       :route "72 North"
+       :direction "Westbound"
+       :eta "4:20"
+       }])
+
 (defn handler [req]
   (router req))
+					; (ns x (:use net.cgrand.enlive-html))
+					; (html-resource "web/index.html")
 
 (def app (-> #'handler
 	     (wrap-file "resources/web")
