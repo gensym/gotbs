b23be172aa1bee307184c8d3ce4c63bf33645e38
David Altenburg <david@gensym.org>
2011-02-22 22:01:46 -0600

    Added rudimentary graphing code. DOes not work.

---
+++ resources/web/js/cta_route_map.js
@@ -0,0 +1,62 @@
+var data = 
+  [
+    { lat: 41.882114939198, lon: -87.625998258591 },
+    { lat: 41.882039054742, lon: -87.629334926605 },
+    { lat: 41.882019085133, lon: -87.630681395531 } ];
+
+function max(list) {
+  if (list.length == 0) {
+    return null;
+  }
+  var ret = list[0];
+  for (var i = 1; i < list.length; i++) {
+    if (list[i] > ret) {
+      ret = list[i];
+    }                        
+  }
+  return ret;
+}
+
+function min(list) {
+  if (list.length == 0) {
+    return null;
+  }
+  var ret = list[0];
+  for (var i = 1; i < list.length; i++) {
+    if (list[i] < ret) {
+      ret = list[i];
+    }                        
+  }
+  return ret;
+}
+
+var canvas = $('#map')[0];
+var width = canvas.width;
+var height = canvas.height;
+
+var lats = $.map(data, function (x) { return x['lat']; });                    
+var max_lat = max(lats);
+var min_lat = min(lats);
+
+var lons = $.map(data, function (x) { return x['lon']; });                    
+var max_lon = max(lons);
+var min_lon = min(lons);
+
+var normalized_data = [];
+
+for (var i = 0; i < data.length; i++) {
+  var datum = {};
+  datum['lon'] = (data[i]['lon'] - min_lon) / (max_lon - min_lon) * width;
+  datum['lat'] = (data[i]['lat'] - min_lat) / (max_lat - min_lat) * height;
+  normalized_data.push(datum);
+}                    
+
+if (canvas.getContext && normalized_data.length > 0) {
+  var ctx = canvas.getContext('2d');
+  ctx.beginPath();
+  ctx.moveTo(normalized_data[0]['lat'], normalized_data[0]['lon']);
+  for (var i = 1; i < normalized_data.length; i++) {
+    ctx.lineTo(normalized_data[i]['lat'], normalized_data[i]['lon']);
+  }
+  ctx.stroke();
+}    
