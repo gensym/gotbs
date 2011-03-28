9fa3036d27206ce1c370c1897dc0f9622f5c925c
David Altenburg <david@gensym.org>
2011-03-27 22:57:58 -0500

    Got tabbing through the routes form working

--- resources/web/js/autocomplete_routes.js
+++ resources/web/js/autocomplete_routes.js
@@ -1,5 +1,7 @@
 
 $(document).ready(function() {
+
+  $('#add-route-button').hide();
   $("input#route-text").autocomplete({
     source: 
     function(request, response) {
@@ -10,16 +12,22 @@ $(document).ready(function() {
   });
 
   $("input#route-text").change(function() {
-    var direction_input = '<div class="available-direction"><input id="route-direction-${i}" name="route-direction" type="radio" class="field radio" value="${direction}" tabindex="${i + 1}" /><label class="choice" for="route-direction-${i}" >${direction}</label></div>';
+    var direction_input = '<div class="available-direction"><input id="route-direction-${i}" name="route-direction" type="radio" class="field radio direction-radio" value="${direction}" tabindex="${i + 1}" /><label class="choice" for="route-direction-${i}" >${direction}</label></div>';
     $(".available-direction").remove();
     $.ajax({
       url:"/routes/directions.json",
       data: {term: this.value},
       success: function(directions) { 
         $.each(directions,  function(i, direction){ 
-
           $.tmpl(direction_input, {"direction": direction, "i": i}).appendTo('#available-route-directions');
         });
+        $('.direction-radio').first().focus();
+        $('#add-route-button').show();
+        $('#route-selection').onSubmite = function() { return true;
+                                              }
+
+        // todo - add form validation - turn submit into an ajax
+        // submit to add routes
       }});
   });
 });
