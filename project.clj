06f505dcb8ef4f03840ab5f82fe981d4818ee840
David Altenburg <david@gensym.org>
2011-06-20 22:55:40 -0500

    Upgraded to jetty 7

--- project.clj
+++ project.clj
@@ -3,9 +3,12 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [midje "0.8.0"]
-		 [ring/ring-core "0.2.5"]
-		 [ring/ring-devel "0.2.5"]
-		 [ring/ring-jetty-adapter "0.2.5"]
+                 [org.signaut/ring-jetty7-adapter "0.3.8"]
+                 [ring/ring-jetty-async-adapter "0.3.3-SNAPSHOT"]
+		 [ring/ring-core "0.3.8"]
+		 [ring/ring-devel "0.3.8"]
 		 [enlive "1.0.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
-		     [lein-run "1.0.0-SNAPSHOT"]])
+		     [lein-run "1.0.0-SNAPSHOT"]]
+
+)
--- src/gotbs/run.clj
+++ src/gotbs/run.clj
@@ -1,5 +1,5 @@
 (ns gotbs.run
-  (use [ring.adapter.jetty])
+  (use [ring.adapter.jetty7])
   (require [gotbs.core]))
 
 (run-jetty #'gotbs.core/app {:port 8080})
