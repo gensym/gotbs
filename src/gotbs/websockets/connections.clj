f362f6f899737ba014fae4752a4baaa10ee5c2e9
David Altenburg <david@gensym.org>
2011-09-14 22:01:39 -0500

    added unsubscribe to connections

--- src/gotbs/websockets/connections.clj
+++ src/gotbs/websockets/connections.clj
@@ -4,23 +4,39 @@
   {:topics-by-connection (ref {})
    :connections-by-topic (ref {})})
 
+(defn- add-topic [connection topic topics-by-connection]
+  (let [topics (get topics-by-connection connection #{})]
+    (assoc topics-by-connection connection (conj topics topic))))
+
+(defn- add-connection [connection topic connections-by-topic]
+  (let [connections (get connections-by-topic topic #{})]
+    (assoc connections-by-topic topic (conj connections connection))))
+
 (defn subscribe [connection-set connection topic]
   (dosync
-   (let [topics (get @(:topics-by-connection connection-set) connection  [])
-         connections (get @(:connections-by-topic connection-set)  topic [])]
-     (alter (:topics-by-connection connection-set)
-            (fn [tbc] (assoc tbc connection (cons topic topics))))
-     (alter (:connections-by-topic connection-set)
-            (fn [cbt] (assoc cbt topic (cons connection connections)))))))
+   (alter (:topics-by-connection connection-set) (partial add-topic connection topic))
+   (alter (:connections-by-topic connection-set) (partial add-connection connection topic))))
+
+(defn- remove-connection [connection topic connections-by-topic]
+  (let [connections (get connections-by-topic topic #{})]
+    (assoc connections-by-topic topic (disj connections connection))))
 
 (defn close [connection-set connection]
-  {})
+  (dosync
+   (dorun
+    (map
+     (fn [topic] (alter
+                 (:connections-by-topic connection-set)
+                 (partial remove-connection connection topic)))
+     (get @(:topics-by-connection connection-set) connection)))
+   (alter (:topics-by-connection connection-set) (fn [m] (dissoc m connection)))
+   ))
+
+(defn send-data [connection data]
+  (io!
+   (.send connection data)))
 
 (defn broadcast [connection-set topic data]
   "Return the number of messages sent"
   (let [connections (get @(:connections-by-topic connection-set) topic)]
     (count (map (fn [conn] (send-data conn data)) connections))))
-
-(defn send-data [connection data]
-  (io!
-   (.send connection data)))
--- test/gotbs/websockets/connections_test.clj
+++ test/gotbs/websockets/connections_test.clj
@@ -3,6 +3,9 @@
         [midje.semi-sweet])
   (:require [clojure [test :as test]]))
 
+(test/deftest should-broadcast-to-nothing-when-nothing-subscribed
+  (expect (broadcast (connection-set) "some-topic" "some data") => 0))
+
 (test/deftest should-broadcast-to-subscribed-connections
   (let [cs (connection-set)
         topic "some-topic"
@@ -13,4 +16,16 @@
     (expect 
      (broadcast cs topic "some data") => 2
      (fake (send-data conn1 "some data") => nil)
+     (fake (send-data conn2 "some data") => nil))))
+
+(test/deftest should-unsubscribe-when-connection-closed
+  (let [cs (connection-set)
+        topic "some-topic"
+        conn1 "conn1"
+        conn2 "conn2"]
+    (subscribe cs conn1 topic)
+    (subscribe cs conn2 topic)
+    (close cs conn1)
+    (expect 
+     (broadcast cs topic "some data") => 1
      (fake (send-data conn2 "some data") => nil))))
