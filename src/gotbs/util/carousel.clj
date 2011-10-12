3538c8a9527bb2235ecddddf709761e94458d43c
David Altenburg <david@gensym.org>
2011-10-11 22:52:49 -0500

    Fleshed out the carousel queues

--- src/gotbs/util/carousel.clj
+++ src/gotbs/util/carousel.clj
@@ -1,13 +1,51 @@
 (ns gotbs.util.carousel)
 
-(deftype Carousel [items]
+(deftype Carousel [items marked unmarked]
+
   clojure.lang.IPersistentStack
-  (cons [this item] (Carousel. (concat items (list item))))
-  (peek [this] (first items))
-  (pop [this] (Carousel. (rest items)))
+
+  (cons [this item]
+        (let [new-queue (if (or
+                             (contains? marked item)
+                             (contains? unmarked item))
+                          items
+                          (conj items item))]
+          (Carousel. new-queue (conj marked item) (disj unmarked item))))
+
+  (peek [this] (first (seq this)))
+
+  (pop [this]
+       (loop [all items
+              m marked
+              um unmarked]
+         (let [item (first all)]
+           (cond
+            (empty? all) (Carousel. clojure.lang.PersistentQueue/EMPTY #{} #{})
+            (contains? m item) (Carousel.
+                                      (conj (pop all) item)
+                                      (disj m item)
+                                      (conj um item))
+            (contains? um item) (recur (conj (pop all) item) m (disj um item))
+            :else (recur (pop all) m um)))))
 
   clojure.lang.Seqable
-  (seq [this] items))
+  (seq [this]
+       (let [filtered (filter #(contains? marked %) items)]
+         (if (empty? filtered) nil filtered))))
 
-(defn make-carousel [items]
-     (Carousel. items))
+(defn poptimes [n coll]
+  (nth (iterate pop coll) n))
+
+(defn make-carousel 
+  "Create a Carousel - a data structure that functions as a circular queue.
+
+    When items are popped and then readded to the queue, their original order is preserved, regardless of the order in which they are readded. However, when the queue has been cycled through without readding the item, the item gets dropped, and if it is then readded, it is placed on the end. Adding an item that is already on the queue has no effect. 
+
+   Why would you want to use such a bizarre structure? Probably because you processing items and scheduling them to be processed on independent timelines, and you want to ensure fairness when the processing gets backed up. 
+"
+  ([]
+     (Carousel. clojure.lang.PersistentQueue/EMPTY #{} #{}))
+  ([items]
+     (if (empty? items)
+       (make-carousel)
+       (Carousel. (apply (partial conj clojure.lang.PersistentQueue/EMPTY) items) (set items) #{}))))
--- test/gotbs/util/carousel_test.clj
+++ test/gotbs/util/carousel_test.clj
@@ -15,17 +15,56 @@
 
 (test/deftest should-map-items
   (let [cq1 (make-carousel (range 5))
-        cq2 (drop 3 cq1)]
-    (expect (map #(do-process %) cq2) => (list true false)
+        cq2 (poptimes 3 cq1)]
+    (expect (map #(do-process %) cq2) => '(true false)
             (fake (do-process 3) => true)
             (fake (do-process 4) => false))))
 
 (test/deftest should-conj-items
   (let [cq (make-carousel (range 5))
         cq2 (conj cq 10 11)]
-    (expect (take 7 cq2) => (list 0 1 2 3 4 10 11))))
+    (expect (take 7 cq2) => '(0 1 2 3 4 10 11))))
 
 (test/deftest should-drop-items
   (let [cq (make-carousel (range 5))
-        cq2 (drop 3 cq)]
-    (expect (take 5 cq2) => (list 3 4))))
+        cq2 (poptimes 3 cq)]
+    (expect (take 5 cq2) => '(3 4))))
+
+(test/deftest should-readd-items
+  (let [cq1 (make-carousel (range 5))
+        cq2 (poptimes 2 cq1)
+        cq3 (conj cq2 1)
+        cq4 (conj cq3 0)]
+    (expect (take 10 cq2) => '(2 3 4))
+    (expect (take 10 cq3) => '(2 3 4 1))
+    (expect (take 10 cq4) => '(2 3 4 0 1))))
+
+(test/deftest should-make-empty-carousel
+  (let [cq (make-carousel)]
+    (expect (empty? cq) => true)
+    (expect (take 10 cq) => '())))
+
+(test/deftest should-make-carousel-from-empty-collection
+  (let [cq (make-carousel (range 0))]
+    (expect (empty? cq) => true)
+    (expect (take 10 cq) => '())))
+
+(defn ignore []
+  (test/deftest should-drop-items-after-passing-through
+    (let [cq1 (make-carousel (range 3))
+          cq2 (pop cq1)
+          cq3 (poptimes 2 cq2)
+          cq4 (poptimes 3 cq3)]
+      (expect (take 3 cq1) => '(0 1 2)))))
+
+
+(test/deftest should-drop-items-after-passing-through
+  (let [cq1 (make-carousel (range 3))
+        cq2 (pop cq1)
+        cq3 (poptimes 2 cq2)
+        cq4 (poptimes 3 cq3)
+        cq5 (-> cq4 (conj 2) (conj 1) (conj 0))]
+    (expect (take 3 cq1) => '(0 1 2))
+    (expect (empty? cq3) => true)
+    (expect (take 3 cq5) => '(2 1 0))))
+
