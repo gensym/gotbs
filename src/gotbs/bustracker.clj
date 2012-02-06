f71267768222c48c805373a9057e52e482ae22c4
David Altenburg <david@gensym.org>
2012-02-05 21:19:18 -0600

    Refactored HTTP interaction with the CTA bustrack into its own type/namespace

--- src/gotbs/bustracker.clj
+++ src/gotbs/bustracker.clj
@@ -1,15 +1,13 @@
 (ns gotbs.bustracker
-  (:import
-   (java.net URL)
-   (java.io BufferedReader InputStreamReader StringBufferInputStream))
   (:use
-   clojure.xml (parse)
+   gotbs.cta.bustracker-api
    [clojure.contrib [logging :as log]] 
    clojure.java.io (input-stream)
    clojure.contrib.str-utils (re-gsub)))
 
 ;; BEGIN HARDCODING
-(def api-key (System/getenv "CTA_BUSTRACKER_API_KEY"))
+
+(def http-api (make-bustracker (System/getenv "CTA_BUSTRACKER_API_KEY")))
 
 (def west-bound "West Bound")
 
@@ -23,57 +21,6 @@
 
 (def destination "Jefferson Park Blue Line")
 ;; END HARDCODING
-
-(defn fetch-url
-  "Returns the contents at a URL as a string"
-  [address]
-  (let [url (URL. address)]
-    (log/info (str "Requesting " url))
-    (with-open [stream (. url openStream)]
-      (let [buf (BufferedReader. (InputStreamReader. stream))]
-        (spy
-         (apply str (line-seq buf)))))))
-
-(defn fetch-routes-data-xml []
-  (fetch-url
-   (str "http://www.ctabustracker.com/bustime/api/v1/getroutes?key=" api-key)))
-
-(defn fetch-route-direction-xml [route]
-  (fetch-url
-   (str "http://www.ctabustracker.com/bustime/api/v1/getdirections?key=" api-key "&rt=" route)))
-
-(defn fetch-location-data-xml [route]
-  (fetch-url
-   (str "http://www.ctabustracker.com/bustime/api/v1/getvehicles?key=" api-key "&rt=" route)))
-
-(defn fetch-prediction-data-xml [route stop-id]
-  (fetch-url
-   (str
-    "http://www.ctabustracker.com/bustime/api/v1/getpredictions?key=" api-key
-    "&rt=" route
-    "&stpid=" stop-id)))
-
-(defn fetch-stop-data-xml [route direction]
-  (let [dir (re-gsub #"\s" "+" direction)]
-    (fetch-url
-     (str "http://www.ctabustracker.com/bustime/api/v1/getstops?key=" api-key "&rt=" route "&dir=" dir))))
-
-(defn fetch-vehicles-on-route-data-xml [route]
-  (fetch-url
-   (str "http://www.ctabustracker.com/bustime/api/v1/getvehicles?key=" api-key "&rt=" route)))
-
-(defn fetch-vehicles-data-xml [vehicles]
-  "Takes a sequence of vehicles (limit 10) and gets their information"
-  (fetch-url
-   (str "http://www.ctabustracker.com/bustime/api/v1/getvehicles?key=" api-key "&vid=" (reduce #(str %1 "," %2) vehicles))))
-
-(defn fetch-pattern-data-for-route-xml [route]
-  (fetch-url
-   (str "http://www.ctabustracker.com/bustime/api/v1/getpatterns?key=" api-key "&rt=" route)))
-
-(defn fetch-pattern-data-by-id-xml [pattern-id]
-  (fetch-url
-   (str "http://www.ctabustracker.com/bustime/api/v1/getpatterns?key=" api-key "&pid=" pattern-id)))
 
 (defn content-xml-to-map [xml]
   "Takes a seq in the form of [{:tag :a :content [\"something\"]} {:tag b :content [\"else\"]}] and turns it into a map, with the value of the :tag as the keys and the values of :content as values. If the value of :content is a list, takes the first item off of that list."
@@ -104,9 +51,7 @@
     (filter-tag
      :stop
      (:content
-      (parse
-       (StringBufferInputStream.
-	(fetch-stop-data-xml route direction))))))))
+      (stops http-api route direction))))))
 
 (defn stop-id [route direction stop-name]
   (first (map :stpid (filter #(= (:stpnm %1) stop-name) (fetch-stop-data route direction)))))
@@ -123,12 +68,14 @@
 	  (filter-tag
 	   :vehicle
 	   (:content
-	    (parse (StringBufferInputStream. vehicle-data-xml)))))))]
+            vehicle-data-xml)))))]
+  
   (defn fetch-vehicles-on-route-data [route]
-    (construct-vehicle-data (fetch-vehicles-on-route-data-xml route)))
+    (construct-vehicle-data (vehicles-on-route http-api route)))
 
   (defn fetch-vehicles-data [& vehicle_ids]
-    (construct-vehicle-data (fetch-vehicles-data-xml vehicle_ids))))
+    (let [vids  (reduce #(str %1 "," %2) vehicle_ids)]
+      (construct-vehicle-data (vehicle-by-id http-api vids)))))
 
 (defn fetch-pattern-data-by-id [pattern-id]
   "Fetch data about the pattern by the pattern ID. Excludes waypoints"
@@ -141,7 +88,7 @@
       (filter-tag
        :ptr
        (:content
-        (parse (StringBufferInputStream. (fetch-pattern-data-by-id-xml pattern-id))))))))))
+        (pattern-by-id http-api pattern-id))))))))
 
 (defn fetch-pattern-data-for-route [route dir]
   "Fetch the pattern data (including waypoints) by the route and direction"
@@ -159,7 +106,7 @@
 	(filter-tag
 	 :ptr
 	 (:content
-	  (parse (StringBufferInputStream. (fetch-pattern-data-for-route-xml route))))))))))))
+          (patterns-for-route http-api route))))))))))
 
 (defn fetch-routes []
   (map
@@ -169,8 +116,7 @@
     (filter-tag
      :route
      (:content
-      (parse (StringBufferInputStream. 
-              (fetch-routes-data-xml))))))))
+      (all-routes http-api))))))
 
 (defn fetch-route-direction [route]
   (flatten
@@ -179,7 +125,7 @@
     (filter-tag
      :dir
      (:content
-      (parse (StringBufferInputStream. (fetch-route-direction-xml route))))))))
+      (directions http-api route))))))
 
 (defn destination [route dir]
   (:stpnm (last (fetch-pattern-data-for-route route dir))))
@@ -192,7 +138,7 @@
     (filter-tag
      :prd
      (:content
-      (parse (StringBufferInputStream. (fetch-prediction-data-xml route stop-id))))))))
+      (predictions http-api route stop-id))))))
 
 (defstruct prediction :route :direction :eta)
 
--- test/gotbs/bustracker_test.clj
+++ test/gotbs/bustracker_test.clj
@@ -1,79 +1,75 @@
 (ns gotbs.bustracker-test
   (:use [gotbs.bustracker] :reload-all
-        [clojure.contrib.mock.test-adapter])
-  (:require [clojure [test :as test]]))
+        [midje.semi-sweet])
+  (:require [clojure.test :as test]
+            [gotbs.cta.bustracker-api :as bt]))
 
 (test/deftest should-fetch-stop-data-for-route
-  (expect
-   [fetch-stop-data-xml
-    (has-args ["56" "North Bound"]
-              (returns (slurp "resources/test-data/stop-data-56-nb.xml")))]
-   (test/is
-    (=
-     {:stpid "5520"
-      :stpnm "Fulton & Desplaines"
-      :lat "41.886871516554"
-      :lon "-87.644065618515"}
-     (first (fetch-stop-data "56" "North Bound"))))))
+  (let [fake-url (Object.)]
+    (expect
+     (first (fetch-stop-data "56" "North Bound")) =>   {:stpid "5520"
+                                                        :stpnm "Fulton & Desplaines"
+                                                        :lat "41.886871516554"
+                                                        :lon "-87.644065618515"}
+     (fake
+      (bt/bustracker-url "stops" anything {"rt" "56", "dir" "North Bound"}) => fake-url)
+     (fake
+      (bt/fetch-url fake-url) =>  (slurp "resources/test-data/stop-data-56-nb.xml")))))
 
 (test/deftest should-fetch-all-routes
-  (expect
-   [fetch-routes-data-xml
-    (has-args []
-              (returns (slurp "resources/test-data/routes.xml")))]
-   (test/is
-    (=
-     {:rtnm "Indiana/Hyde Park"
-      :rt "1"})
-    (first (fetch-routes)))))
+  (let [fake-url (Object.)]
+    (expect (first (fetch-routes)) => {:rtnm "Indiana/Hyde Park"
+                                       :rt "1"}
+            (fake (bt/bustracker-url "routes" anything) => fake-url)
+            (fake (bt/fetch-url fake-url) =>  (slurp "resources/test-data/routes.xml")))))
 
 (test/deftest should-fetch-route-direction
-  (expect
-   [fetch-route-direction-xml
-    (has-args ["56"]
-              (returns (slurp "resources/test-data/route-direction-56.xml")))]
-   (test/is
-    (= ["North Bound" "South Bound"]
-         (fetch-route-direction "56")))))
+  (let [fake-url (Object.)]
+    (expect (fetch-route-direction "56") =>  ["North Bound" "South Bound"]
+            (fake (bt/bustracker-url "directions" anything {"rt" "56"}) => fake-url)
+            (fake (bt/fetch-url fake-url) =>
+                  (slurp "resources/test-data/route-direction-56.xml")))))
 
 (test/deftest should-fetch-pattern-data-by-route
-  (expect
-   [fetch-pattern-data-for-route-xml
-    (has-args ["56"]
-              (returns (slurp "resources/test-data/pattern-data-for-route-56.xml")))]
-   (test/is
-    (=
-     (list
-      {:seq "1"
-       :lat "41.882130914862"
-       :lon "-87.625998258591"
-       :typ "S"
-       :stpid "450"
-       :stpnm "Madison & Wabash"
-       :pdist "0.0"
-       },
-      {:seq "2"
-       :lat "41.882118933114",
-       :lon "-87.62768805027"
-       :typ "S"
-       :stpid "4729",
-       :stpnm "Madison & State"
-       :pdist "654.0"},
-      {:seq "3"
-       :lat "41.882063018264",
-       :lon "-87.629334926605",
-       :typ "W"})
-     (take 3
-           (fetch-pattern-data-for-route "56" "North Bound"))))))
+  (let [fake-url (Object.)]
+    (expect (take 3
+                  (fetch-pattern-data-for-route "56" "North Bound")) =>
+                  (list
+                   {:seq "1"
+                    :lat "41.882130914862"
+                    :lon "-87.625998258591"
+                    :typ "S"
+                    :stpid "450"
+                    :stpnm "Madison & Wabash"
+                    :pdist "0.0"
+                    },
+                   {:seq "2"
+                    :lat "41.882118933114",
+                    :lon "-87.62768805027"
+                    :typ "S"
+                    :stpid "4729",
+                    :stpnm "Madison & State"
+                    :pdist "654.0"},
+                   {:seq "3"
+                    :lat "41.882063018264",
+                    :lon "-87.629334926605",
+                    :typ "W"})
+                  (fake
+                   (bt/bustracker-url "patterns" anything {"rt" "56"}) =>
+                   fake-url)
+                  (fake
+                   (bt/fetch-url fake-url) =>
+                   (slurp "resources/test-data/pattern-data-for-route-56.xml")))))
 
-(test/deftest should-fetch-pattern-data-for-route
-  (expect
-   [fetch-pattern-data-by-id-xml
-    (has-args ["1970"]
-              (returns (slurp "resources/test-data/pattern-data-by-id-1970.xml")))]
-   (test/is
-    (= 
-     {:rtdir "North Bound",
-      :ln "51451.0"
-      :pid "1970"}
-     (fetch-pattern-data-by-id "1970")))))
+(test/deftest should-fetch-pattern-data-by-id
+  (let [fake-url (Object.)]
+    (expect (fetch-pattern-data-by-id "1970") => {:rtdir "North Bound",
+                                                  :ln "51451.0"
+                                                  :pid "1970"}
+            (fake
+             (bt/bustracker-url "patterns" anything {"pid" "1970"}) =>
+             fake-url)
+            (fake
+             (bt/fetch-url fake-url) =>
+              (slurp "resources/test-data/pattern-data-by-id-1970.xml")))))
+
