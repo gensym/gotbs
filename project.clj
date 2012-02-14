d2f773454489c3db87ec53a8bd0617fe61bca2a6
David Altenburg <david@gensym.org>
2012-02-13 21:51:05 -0600

    Added clj-time dep.

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.clojure/data.json "0.1.2"]
+                 [org.clojure/core.cache "0.5.0"]
                  [midje "1.3.2-SNAPSHOT"]
                  [org.webbitserver/webbit "0.2.0"]
                  [org.eclipse.jetty/jetty-servlet "7.4.2.v20110526"]
@@ -11,8 +12,8 @@
 		 [ring/ring-core "1.0.1"]
                  [ring/ring-jetty-adapter "1.0.1"]
 		 [ring/ring-devel "1.0.1"]
-		 [enlive "1.0.0-SNAPSHOT"]
-                 ]
+                 [clj-time "0.3.5"]
+		 [enlive "1.0.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.4.0"]]
 
   :main gotbs.run)
