e29c66aa880ed4a5e55564c908632ba61bcd15e0
David Altenburg <david@gensym.org>
2012-03-26 21:43:12 -0500

    upgrade to lein 2

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject gotbs "0.0.1"
-  :description "CTA Bus Tracker predictions for multi-leg commutes"
+(defproject gotbs "0.0.1" 
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.clojure/data.json "0.1.2"]
@@ -7,13 +6,16 @@
                  [org.clojure/core.memoize "0.5.1"]
                  [org.webbitserver/webbit "0.2.0"]
                  [org.eclipse.jetty/jetty-servlet "7.4.2.v20110526"]
-		 [ring/ring-core "1.0.1"]
+                 [ring/ring-core "1.0.1"]
                  [ring/ring-jetty-adapter "1.0.1"]
-		 [ring/ring-devel "1.0.1"]
+                 [ring/ring-devel "1.0.1"]
                  [clj-time/clj-time "0.3.7"]
-		 [enlive "1.0.0-SNAPSHOT"]]
-  :dev-dependencies [[swank-clojure "1.4.0"]
-                     [midje "1.3.2-SNAPSHOT"]
-                     [org.gensym/tools.trace "0.1"]]
-
-  :main gotbs.run)
+                 [enlive "1.0.0-SNAPSHOT"]]
+  :profiles {:dev
+             {:dependencies
+              [[midje "1.3.2-SNAPSHOT"]
+               [org.gensym/tools.trace "0.1"]]}}
+  :main gotbs.run
+  :min-lein-version "2.0.0"
+  :plugins [[lein-swank "1.4.1"]]
+  :description "CTA Bus Tracker predictions for multi-leg commutes")
